5b45814441f256325cb31723e52af579951d1f6a
peterdinis <petter1@pobox.sk>
2025-06-29 13:30:06 +0200

    feat: Basic landing page is DONE

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import type { NextPage } from "next";
 import FeaturesSection from "~/components/hero/FeaturesSection";
 import HeroWrapper from "~/components/hero/HeroWrapper";
+import Footer from "~/components/shared/Footer";
 
 const Homepage: NextPage = () => {
 	return (
 		<>
 			<HeroWrapper />
 			<FeaturesSection />
+			<Footer />
 		</>
 	);
 };
