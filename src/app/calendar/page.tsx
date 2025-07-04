74c6e7fcac189ec5023ff20918b30ac1fea20ad0
peterdinis <petter1@pobox.sk>
2025-07-04 16:30:23 +0200

    feat: Working on calendar page

---
+++ src/app/calendar/page.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next";
+import CalendarWrapper from "~/components/calendar/CalendarWrapper";
+
+const CalendarPage: NextPage = () => {
+    return <CalendarWrapper />
+}
+
+export default CalendarPage;
---
+++ src/components/calendar/CalendarWrapper.tsx
@@ -0,0 +1,9 @@
+import type { FC } from "react";
+
+const CalendarWrapper: FC = () => {
+    return (
+        <>ABCD</>
+    )
+}
+
+export default CalendarWrapper
