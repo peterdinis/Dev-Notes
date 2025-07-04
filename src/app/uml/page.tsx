e10b0b66db6d5d7a280fac10dbc13723d77d3a1e
peterdinis <petter1@pobox.sk>
2025-07-04 18:35:24 +0200

    feat: Start working on new uml page

---
+++ src/app/uml/page.tsx
@@ -0,0 +1,8 @@
+import type { NextPage } from "next"
+import UmlWrapper from "~/components/uml/UmlWrapper"
+
+const UMLPage: NextPage = () => {
+    return <UmlWrapper />
+}
+
+export default UMLPage
