3d0336589fb3b6c531be7033f2ba977d4e1128b9
peterdinis <petter1@pobox.sk>
2025-07-10 21:15:03 +0200

    feat: Updating auth code

--- src/components/auth/ProfileDropdown.tsx
+++ src/components/auth/ProfileDropdown.tsx
@@ -2,6 +2,7 @@
 
 import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
 import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import Link from "next/link";
 import type { FC } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "~/components/ui/avatar";
 import { Button } from "~/components/ui/button";
@@ -31,9 +32,13 @@ const ProfileDropdown: FC = () => {
 				sideOffset={5}
 				align="end"
 			>
-				<div className="mb-2 border-b p-2">
-					<p className="text-muted-foreground text-xs">{user?.email}</p>
-				</div>
+				<DropdownMenuItem className="mb-2 border-b p-2">
+					<p className="text-muted-foreground">{user?.email}</p>
+				</DropdownMenuItem>
+
+				<DropdownMenuItem className="mb-2 border-b p-2">
+					<Link className="text-muted-foreground" href="/dashboard">Dashboard</Link>
+				</DropdownMenuItem>
 
 				<DropdownMenuItem className="cursor-pointer rounded px-2 py-1">
 					<LogoutLink href="/login">Logout</LogoutLink>
