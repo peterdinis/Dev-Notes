10e0cbc54c0485ae7e50b1bd8c7b0df6ec8ac76f
peterdinis <petter1@pobox.sk>
2025-07-10 21:18:51 +0200

    FOO

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,6 +1,6 @@
 import { sql } from "drizzle-orm";
 import { relations } from "drizzle-orm";
-import { sqliteTableCreator, text } from "drizzle-orm/sqlite-core";
+import { sqliteTableCreator, text } from "drizzle-orm/sqlite-core"; 
 
 export const createTable = sqliteTableCreator((name) => `dev-notes_${name}`);
 
